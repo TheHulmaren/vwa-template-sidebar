30b67ccf547ed5cf24dced0254e3d1d8b0d0c907
TheHulmaren <rbalsdldi@naver.com>
2023-03-23 20:09:13 +0900

    Added locomotive scroll

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <div id="sideBar" class="from-right slide-in-out" :class="{opened:sidebarOpened}">
-  <div id="sideBarContent">
-    Sidebar
+  <div
+    id="sideBar"
+    class="from-right slide-in-out"
+    :class="{ opened: sidebarOpened }"
+  >
+    <div id="sideBarContent">Sidebar</div>
   </div>
-  </div>
-  <div id="appWrapper">
+  <div data-scroll-container id="appWrapper">
     <header>
       <button @click="toggleSidebar()">MENU</button>
       Header
     </header>
-    <main>Main</main>
-    <footer>Footer</footer>
+    <div data-scroll-section id="layoutScrollBox">
+      <main>
+        Main
+        <div style="height: 3000px"></div>
+      </main>
+      <footer>Footer</footer>
+    </div>
   </div>
 </template>
 <script>
+import LocomotiveScroll from "locomotive-scroll";
+
 export default {
   name: "App",
   data() {
@@ -25,6 +34,12 @@ export default {
     toggleSidebar() {
       this.sidebarOpened = !this.sidebarOpened;
     },
+  },
+  mounted() {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+    });
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/css/layout.css";
-import "./assets/css/sidebar.css"
+import "./assets/css/sidebar.css";
+import "./assets/css/locomotive-scroll.css"
 
 const app = createApp(App);
 
